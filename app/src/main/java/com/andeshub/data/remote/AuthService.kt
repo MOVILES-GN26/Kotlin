ba6a85208a09dfc728207b5f22807fc1714da199
santi-pine03 <s.pinedaq@uniandes.edu.co>
2026-03-19 14:59:26 -0500

    ViewModel for Auth

---
+++ app/src/main/java/com/andeshub/data/remote/AuthService.kt
@@ -0,0 +1,15 @@
+package com.andeshub.data.remote
+
+import com.andeshub.data.model.AuthResponse
+import com.andeshub.data.model.LoginRequest
+import com.andeshub.data.model.RegisterRequest
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthService {
+    @POST("auth/login")
+    suspend fun login(@Body request: LoginRequest): AuthResponse
+
+    @POST("auth/register")
+    suspend fun register(@Body request: RegisterRequest): AuthResponse
+}
---
+++ app/src/main/java/com/andeshub/ui/auth/AuthViewModel.kt
@@ -0,0 +1,7 @@
+package com.andeshub.ui.auth
+
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+
+}
