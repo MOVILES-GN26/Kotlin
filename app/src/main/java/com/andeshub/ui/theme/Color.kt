f102c016594a1210fd23226588dc53f58ef06171
Julianaba7 <mj.ballesteros@uniandes.edu.co>
2026-03-18 20:18:14 -0500

    colors

--- app/src/main/java/com/andeshub/ui/theme/Color.kt
+++ app/src/main/java/com/andeshub/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.andeshub.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SmallBackground = Color(0xFFF2F2E8)
+val Black = Color(0xFF1C1A0D)
+val BackgroundWhite = Color(0xFFFCFAF7)
+val Yellow = Color(0xFFF0E342)
+val OliveGreen = Color(0xFF99944D)
