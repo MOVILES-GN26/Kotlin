35aab36825f488fd97ac64dcaade94b954b7daea
Julianaba7 <mj.ballesteros@uniandes.edu.co>
2026-03-19 00:19:53 -0500

    Bottom Navigation Bar

---
+++ app/src/main/java/com/andeshub/routes/AppDestinations.kt
@@ -0,0 +1,9 @@
+package com.andeshub.routes
+
+sealed class AppDestinations(val route: String) {
+    object Home      : AppDestinations("home")
+    object Catalog   : AppDestinations("catalog")
+    object Post      : AppDestinations("post")
+    object Favorites : AppDestinations("favorites")
+    object Profile   : AppDestinations("profile")
+}
