43c514b1b0062502687c2621a405ff9bf7898817
Julianaba7 <mj.ballesteros@uniandes.edu.co>
2026-03-19 19:53:34 -0500

    create store 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0") //store repository
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }
